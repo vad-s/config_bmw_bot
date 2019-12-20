350d6462ec23f4dd372951143af146e9ae7c5254
Vadim S <vadim.stebakov@gmail.com>
2019-12-20 17:09:09 +0300

    final

--- js.js
+++
@@ -1,5 +0,0 @@
-var t = new Date();
-const newDate = t.now();
-console.log(newDate);
-
-console.log(newDate.getTime());
--- runner.js
+++ runner.js
@@ -1,33 +1,31 @@
+const http = require('http');
+const fs = require('fs');
 const FileParser = require('./models/fileParser');
 const LinkMaker = require('./models/linkMaker');
 
-const http = require('http');
-const fs = require('fs');
 
 const savePath = './imported/file2.xlsx';
 const getPath = 'http://f0376840.xsph.ru/4196946.xlsx';
 
-async function saveFile(pathToSave, pathToGet) {
-  const file = fs.createWriteStream(pathToSave);
-  http.get(pathToGet, function(response) {
-    response.pipe(file);
-  })
+async function parse(pathToSave, pathToGet) {
+  const parsed = await new FileParser(savePath).createResultCollection();
+  const link = await new LinkMaker(parsed).linkMaker();
+  return link;
 }
 
+async function saveAndParse(pathToSave, pathToGet) {
+  const file = await fs.createWriteStream(pathToSave);
+  const take = await http.get(pathToGet, async (response) => {
+    await response.pipe(file);
+    response.on('end', async () => {
+      // console.log("File Saved");
 
-// saveFile(savePath, getPath);
-
-async function parseFile(path) {
-  const parser = new FileParser(path);
-  return  parser.createResultCollection();
+      const resultParse = await parse(savePath, getPath);
+      console.log(resultParse);
+    });
+  });
+  // console.log(take);
+  return take;
 }
 
-let parsed = parseFile(savePath);
-
-const linker = new LinkMaker(parsed);
-const result = linker.linkMaker();
-
-console.log(result);
-
-
-
+saveAndParse(savePath, getPath);
